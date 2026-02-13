40cdfcecda830a4f29916d0cae9b912a0a8dc724
priyans11 <priyanshu82711@gmail.com>
2026-02-13 19:43:19 +0530

    12-season-activity,js

--- src/12-season-activity.js
+++ src/12-season-activity.js
@@ -32,4 +32,60 @@
  */
 export function getSeasonActivity(month, temperature) {
   // Your code here
+  let season="";
+  let activity="";
+  if(month<1 || month>12)
+  {
+    return null;
+  }
+  if(month==1 || month==2 || month==12)
+  {
+    season="Winter";
+  }
+  else if(month==3 || month==4 || month==5)
+  {
+    season="Spring";
+  }
+  else if(month==6 || month==7 || month==8)
+  {
+    season="Summer";
+  }
+  else if(month==9 || month==10 || month==11)
+  {
+    season="Autumn";
+  }
+if(season=="Winter" && temperature<0)
+{
+  return {season ,activity:"skiing"};
+}
+else if(season=="Winter" && temperature>=0)
+{
+  return {season,activity:"ice skating"};
+}
+if(season=="Spring" && temperature>20)
+{
+  return {season ,activity:"hiking"};
+}
+else if(season=="Spring" && temperature<=20)
+{
+  return {season ,activity:"museum visit"};
+}
+if(season=="Summer" && temperature>35)
+{
+  return {season ,activity:"swimming"};
+}
+else if(season=="Summer" && temperature<=35)
+{
+  return {season ,activity:"cycling"};
+}
+if(season=="Autumn" && temperature>15)
+{
+  return {season ,activity:"nature walk"};
+}
+else if(season=="Autumn" && temperature<=15)
+{
+  return {season ,activity:"reading at a cafe"};
+}
+
+
 }
