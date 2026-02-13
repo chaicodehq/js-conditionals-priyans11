573cb4f9f724e3805d166ca8f73f3afa7a8cbe3b
priyans11 <priyanshu82711@gmail.com>
2026-02-13 18:30:39 +0530

    07-coffee-shop.js

--- src/08-tax-calculator.js
+++ src/08-tax-calculator.js
@@ -27,4 +27,24 @@
  */
 export function calculateTax(income) {
   // Your code here
+  if(income==0 || income<0)
+  {
+    return 0;
+  }
+  else if(income<10000)
+  {
+    return income*0/100;
+  }
+  else if(income>=10001 && income<30000)
+  {
+    return income*10/100
+  }
+  else if(income>=30001 && income<70000)
+  {
+    return income*20/100
+  }
+  else if(income>=70001)
+  {
+    return income*30/100
+  }
 }
