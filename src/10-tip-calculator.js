5d5bbbdd4b75bdf3c5f9753801d01f1422e0ddaf
priyans11 <priyanshu82711@gmail.com>
2026-02-13 19:21:03 +0530

    10-tip-calculator.js

--- src/10-tip-calculator.js
+++ src/10-tip-calculator.js
@@ -31,4 +31,40 @@
  */
 export function calculateTip(billAmount, serviceRating) {
   // Your code here
+  if(billAmount <= 0)
+    {
+      return null;
+    }
+  let tipPercentage=0;
+  if(serviceRating<1 || serviceRating>5)
+    {
+      return null;
+    }
+  if(serviceRating===1)
+    {
+      tipPercentage=5;
+    }
+  else if(serviceRating===2)
+    {
+      tipPercentage=10;
+    }
+  else if(serviceRating===3)
+    {
+      tipPercentage=15;
+    }
+  else if(serviceRating===4)
+    {
+      tipPercentage=20;
+    }
+  else if(serviceRating===5)
+    {
+      tipPercentage=25;
+    }
+  else
+    {
+      return null;
+    }
+  let tipAmount =(billAmount * tipPercentage / 100);
+  let totalAmount =(billAmount + tipAmount);
+  return { tipPercentage: tipPercentage, tipAmount: tipAmount, totalAmount: totalAmount };
 }
